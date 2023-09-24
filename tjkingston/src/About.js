c864f14d3343d30f2fb898e18308a67ab819a837
Like19A91Bug <bugnificent.tk@gmail.com>
2023-09-24 13:20:12 -0400

    stuff

--- tjkingston/src/About.js
+++ tjkingston/src/About.js
@@ -3,10 +3,17 @@ const About = () => {
         <section id="about">
             <h1>About</h1>
             <h5>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deserunt minus apple perspiciatis, cumque eaque nulla enim,
-                quaerat dolor neque, incidunt Pepsi mollitia labore aspernatur
-                atque magnam. Soluta orange sit dolores sapiente ullam in.
+                Dedicated and adaptable professional with diverse experience in
+                various roles, demonstrating strong leadership and
+                organizational skills. Excels in handling high volumes of tasks
+                efficiently and ensuring goals are met. Proven ability to manage
+                teams and coordinate complex projects. Adept at both field and
+                office tasks.
+                <br />
+                These roles highlight my ability to adapt to diverse
+                responsibilities, lead teams, and achieve goals efficiently. My
+                experience in both field and office environments showcases my
+                versatility and dedication to delivering exceptional results.
             </h5>
         </section>
     );
